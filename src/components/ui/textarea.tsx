92836ff73a66b7f7e9a528ae47af751fca5e6ac8
Malens39 <marcovalentini@outlook.com>
2025-10-30 16:32:22 +0100

    Update textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 export function Input(props: any) {
   return <input {...props} className={`border rounded-xl px-3 py-2 text-sm w-full ${props.className||''}`}/>;
