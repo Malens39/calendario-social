c857f3a53fe8e7f4571e38085f7ef2008cbf2443
Malens39 <marcovalentini@outlook.com>
2025-10-30 16:18:42 +0100

    Create checkbox.tsx

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+export function Checkbox({ checked, onCheckedChange }: { checked: boolean; onCheckedChange: (v:boolean)=>void }) {
+  return <input type="checkbox" checked={checked} onChange={e=>onCheckedChange(e.target.checked)} />;
+}
