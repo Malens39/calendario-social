0a96e79c47365232663601186f3ac394e11ee677
Malens39 <marcovalentini@outlook.com>
2025-10-30 16:32:00 +0100

    Update switch.tsx

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 export function Switch({ checked, onCheckedChange }: { checked: boolean; onCheckedChange: (v:boolean)=>void }) {
   return <input type="checkbox" checked={checked} onChange={e=>onCheckedChange(e.target.checked)} />;
