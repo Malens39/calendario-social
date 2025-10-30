c22eb5df4e544357e163f3af0470d024f837328e
Malens39 <marcovalentini@outlook.com>
2025-10-30 16:17:53 +0100

    Create card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export function Card({ className='', ...props }: any) {
+  return <div className={`rounded-2xl border bg-white ${className}`} {...props}/>;
+}
+export function CardContent({ className='', ...props }: any) {
+  return <div className={`p-4 ${className}`} {...props}/>;
+}
