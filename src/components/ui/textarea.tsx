bd22f81450939ef5c4f34d0273a6f74b1a13cf15
Malens39 <marcovalentini@outlook.com>
2025-10-30 16:18:11 +0100

    Create textarea.tsx

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+export function Input(props: any) {
+  return <input {...props} className={`border rounded-xl px-3 py-2 text-sm w-full ${props.className||''}`}/>;
+}
